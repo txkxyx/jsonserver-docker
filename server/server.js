7f2623fc4fffd2e56081465c3e2fb843fc387002
takuya_okamoto <takuya_okamoto@kronos.jp>
2020-10-10 11:46:35 +0900

    update auth

--- server/server.js
+++ server/server.js
@@ -1,81 +1,94 @@
-//モジュール参照
-const fs = require('fs')
-const bodyParser = require('body-parser')
-const jsonServer = require('json-server')
-const jwt = require('jsonwebtoken')
+const jsonServer = require("json-server");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const helmet = require("helmet");
 
-//JSON Serverで、利用するJSONファイルを設定
-const server = jsonServer.create()
-const router = jsonServer.router('./data/data.json')
+// JSON Serverで使用するJSONファイルを設定
+const server = jsonServer.create();
+const router = jsonServer.router("./data/data.json");
+// JSONリクエスト対応
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
-//JSONリクエスト対応
-server.use(bodyParser.urlencoded({extended: true}))
-server.use(bodyParser.json())
+// 署名
+const JWT_SECRET = "jwt_json_server";
+// 有効時間
+const EXPIRATION = "1h";
+// Cookie
+// const KEY = "token";
 
-//署名作成ワードと有効期限(1時間)
-const SECRET_WORD = 'SECRET1234'
-const expiresIn = '1h'
+// データの操作
+const db = JSON.parse(fs.readFileSync("./data/data.json", "UTF-8"));
+// 記事の検索
+const getBlog = (id) => db.blogs.find((blog) => blog.id === Number(id));
 
-//署名作成関数
-const createToken = payload => jwt.sign(payload, SECRET_WORD, {expiresIn})
+// ログイン認証なしのルーター
+server.post("/auth/login", (req, resp) => {
+  const { email, password } = req.body;
+  // ログイン検証
+  if (
+    db.users.findIndex(
+      (user) => user.email === email && user.password === password
+    ) === -1
+  ) {
+    resp.status(401).json("Unauthorized");
+    return;
+  }
 
-    //署名検証関数（非同期）
-    const verifyToken = token =>
-    new Promise((resolve, reject) =>
-        jwt.verify(token, SECRET_WORD, (err, decode) =>
-        decode !== undefined ? resolve(decode) : reject(err)
-        )
-    )
+  // ログイン後、アクセストークンの生成
+  const access_token = jwt.sign({ email, password }, JWT_SECRET, {
+    expiresIn: EXPIRATION,
+  });
 
-    //ユーザDBファイル読み込み
-    const userdb = JSON.parse(fs.readFileSync('./data/users.json', 'UTF-8'))
+  resp.status(200).json({ access_token });
+  // .cookie(KEY, access_token, {
+  //   httpOnly: true,
+  //   secure: true,
+  // })
+  // .json();
+});
 
-    //ログイン関数 true:ok false:ng
-    const isAuth = ({email, password}) =>
-    userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
+server.get("/blogs", (req, resp) => {
+  const blogs = db.blogs;
+  resp.status(200).json({ blogs });
+});
 
-    //ログインRouter
-    server.post('/auth/login', (req, res) => {
-    const {email, password} = req.body
+server.get("/blog/:id", (req, resp) => {
+  const id = req.params.id;
+  const blog = getBlog(id);
+  resp.status(200).json({ blog });
+});
 
-    //ログイン検証
-    if (isAuth({email, password}) === false) {
-        const status = 401
-        const message = 'Incorrect email or password'
-        res.status(status).json({status, message})
-        return
-    }
+// 認証が必要なルーター
+server.use((req, resp, next) => {
+  // 認証形式チェック
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization.split(" ")[0] !== "Bearer"
+    // req.headers.cookie === undefined ||
+    // !req.headers.cookie.startsWith(KEY)
+  ) {
+    resp.status(401).json("Unauthorized");
+    return;
+  }
 
-    //ログイン成功時に認証トークンを発行
-    const access_token = createToken({email, password})
-    res.status(200).json({access_token})
-    })
+  // 認証チェック
+  try {
+    var decode = jwt.verify(
+      req.headers.authorization.split(" ")[1],
+      //   req.headers.cookie.split(KEY + "=")[1],
+      JWT_SECRET
+    );
+    next();
+  } catch (e) {
+    resp.status(401).json("Unauthorized");
+  }
+});
 
-    //認証が必要なRouter(ログイン以外全て)
-    server.use(/^(?!\/auth).*$/, async (req, res, next) => {
-
-    //認証ヘッダー形式検証
-    if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-        const status = 401
-        const message = 'Error in authorization format'
-        res.status(status).json({status, message})
-        return
-    }
-
-    //認証トークンの検証
-    try {
-        await verifyToken(req.headers.authorization.split(' ')[1])
-        next()
-    } catch (err) {
-        //失効している認証トークン
-        const status = 401
-        const message = 'Error access_token is revoked'
-        res.status(status).json({status, message})
-    }
-})
-
-//認証機能付きのREST APIサーバ起動
-server.use(router)
+// JSON Serverを起動する
+server.use(router);
+server.use(helmet);
 server.listen(33000, () => {
-    console.log('Run Auth API Server')
-})
+  console.log("JSON Server Start");
+});
